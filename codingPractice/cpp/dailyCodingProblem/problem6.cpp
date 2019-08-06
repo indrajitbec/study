3a4f08871ab81e47b1602830f8d26457ede233cc
indmitra <indrajit.mitra@keysight.com>
2019-08-07 01:55:39 +0530

    solution for problem5-8

---
+++ codingPractice/cpp/dailyCodingProblem/problem6.cpp
@@ -0,0 +1,83 @@
+// This problem was asked by Google.
+
+// An XOR linked list is a more memory efficient doubly linked list. 
+// Instead of each node holding next and prev fields, it holds a field named both,
+// which is an XOR of the next node and the previous node.
+// Implement an XOR linked list; 
+// it has an add(element) which adds the element to the end, and 
+// a get(index) which returns the node at index.
+
+// If using a language that has no pointers (such as Python), 
+// you can assume you have access to get_pointer and dereference_pointer functions that converts between nodes and memory addresses.
+
+
+#include<iostream>
+using namespace std;
+
+class XORListNode {
+    public:
+        int val;
+        XORListNode *both;
+        XORListNode(int val, XORListNode *both = nullptr) : val(val), both(both) {}
+};
+
+class XORList {
+    private:
+        XORListNode *head, *tail;
+        XORListNode *XOR(XORListNode *a, XORListNode *b) {
+            return ( XORListNode *)(((unsigned) a) ^ ((unsigned) b));
+        }
+    public:
+        XORList() : head(nullptr), tail(nullptr) {}
+
+        void AddElement(int item) {
+            auto node = new XORListNode(item);
+            if(head == nullptr) {
+                head = tail = node;
+                return;
+            }
+
+            node->both = XOR(tail, nullptr);
+            tail->both = XOR(tail->both, node);
+            tail = node;
+
+            return;
+        }
+
+        int GetElement(int index) {
+            XORListNode *previous = nullptr;
+            auto current = head;
+            while(current && index--) {
+                previous = current;
+                current = XOR(current->both , previous);
+            }
+
+            if(index != 0) {
+                throw std::out_of_range("index out of range");
+            }
+
+            return previous->val;
+        }
+};
+
+int main() {
+
+    XORList list;
+    list.AddElement(1);
+    list.AddElement(2);
+    list.AddElement(3);
+    list.AddElement(4);
+    list.AddElement(5);
+
+    
+
+    try {
+        cout << list.GetElement(3) << endl;
+        //cout << list.GetElement(5) << endl;
+        //cout << list.GetElement(6) << endl;
+    }
+    catch(std::exception &e) {
+        cout << e.what() << endl;
+    }
+    return 0;
+}
---
+++ codingPractice/cpp/dailyCodingProblem/problem8.cpp
@@ -0,0 +1,76 @@
+// This problem was asked by Google.
+
+// A unival tree (which stands for "universal value") is a tree 
+
+// where all nodes under it have the same value.
+
+// Given the root to a binary tree, count the number of unival subtrees.
+
+// For example, the following tree has 5 unival subtrees:
+
+//    0
+//   / \
+//  1   0
+//     / \
+//    1   0
+//   / \
+//  1   1
+
+#include<iostream>
+#include<limits>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node *left, *right;
+    Node(int val, Node *left=nullptr, Node *right=nullptr) {
+        this->val = val;
+        this->left = left;
+        this->right = right;
+    }
+};
+
+class UniversalSubTree {
+private:
+    static int CountRec(Node *root, int &match) {
+        if(root == nullptr) {
+            return 0;
+        }
+        int lmatch = numeric_limits<int>::max();
+        int rmatch = numeric_limits<int>::max();
+        int lc = root->left ? CountRec(root->left, lmatch): 0;
+        int rc = root->right ? CountRec(root->right, rmatch): 0;
+        if(root->left == nullptr && root->right == nullptr) {
+            match = root->val;
+            return 1;
+        } else {
+            if(root->left == nullptr && rc && root->right->val == root->val && root->val == rmatch) {
+                return rc + 1;
+            } else if(root->right == nullptr && lc && root->left->val == root->val && root->val == lmatch) {
+                return lc + 1;
+            } else if(lc && rc && 
+                      (root->left->val == root->val && root->val == lmatch) && 
+                      (root->right->val == root->val & root->val == rmatch) ) { 
+                return lc + rc + 1;
+            }
+        }
+        return lc + rc;
+    }
+public:
+    static int Count(Node *root) {
+        int match = numeric_limits<int>::max();
+        return CountRec(root, match);
+    }
+
+};
+
+int main() {
+    Node *root  = new Node(0, new Node(1) , new Node(0,new Node(1,new Node(1), new Node(1)), new Node(0)));
+
+    cout << UniversalSubTree::Count(root) << endl;
+
+    root  = new Node(0, new Node(1) , new Node(0, new Node(0, new Node(1,new Node(1), new Node(1)), nullptr), new Node(0)));
+
+    cout << UniversalSubTree::Count(root) << endl;
+}
