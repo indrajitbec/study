857a760c47a67efee60c5133e3f902b6aca7b70c
indmitra <indrajit.mitra@keysight.com>
2019-08-11 23:38:23 +0530

    problem 13

---
+++ codingPractice/cpp/dailyCodingProblem/problem13.cpp
@@ -0,0 +1,45 @@
+// This problem was asked by Amazon.
+
+// Given an integer k and a string s, 
+
+// find the length of the longest substring that contains at most k distinct characters.
+
+// For example, given s = "abcba" and k = 2, 
+// the longest substring with k distinct characters is "bcb".
+
+#include<iostream>
+#include<string>
+#include<unordered_map>
+
+using namespace std;
+
+class LongestDistinctSubString {
+public:
+
+    static int findLongest(string str, int k) {
+        unordered_map<char, int> cfreq;
+
+        int wStart=0;
+        int n = str.length();
+        int maxLen = 0;
+
+        for(int wEnd=0; wEnd < n; wEnd++) {
+            cfreq[str[wEnd]]++;
+
+            while(cfreq.size()> k) {
+                char left = str[wStart];
+                cfreq[left]--;
+                if(cfreq[left]==0) {
+                    cfreq.erase(left);
+                }
+                wStart++;
+            }
+            maxLen = max(maxLen, wEnd-wStart+1);
+        }
+        return maxLen;
+    }
+};
+
+int main() {
+    cout << LongestDistinctSubString::findLongest("abcba",2) << endl;
+}
